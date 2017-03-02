30a75a6fd1fc17401b0a5e81971d3acbfba2db1e
Julian Reyes Romero <julian.reyes.romero@ericsson.com>
2017-03-02 17:51:27 +0100

    Scripts to create a cluster with docker engine in swarm mode

---
+++ docker-compose/deploy_swarm_cluster.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Create swarm cluster:
+#   - 1 master node
+#   - 3 workers
+#
+manager_node=1
+workers_node=3
+
+# First step. Create a dump of our application using docker-compose
+# We need to pull all the images indicate in docker-compose.yml for that
+docker-compose pull
+docker-compose bundle
+
+source ./create_swarm_mode_cluster.sh $manager_nodes $workers_nodes
+
+
+# Once the cluster has been created, we need to point Docker Cli to swarm master node
+eval $(docker-machine env master1)
+
+
